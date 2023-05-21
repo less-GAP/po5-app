574184014303da3e7ce51f34e178683ffe96ee55
hungndl <hungndl86@gmail.com>
2023-05-21 23:09:19 +0700

    update videos

--- app/services/spree/admin/upload_video_service.rb
+++ app/services/spree/admin/upload_video_service.rb
@@ -10,10 +10,22 @@ class Spree::Admin::UploadVideoService
       upload = @client.upload_video(video_file)
       video_uri = upload['uri']
       File.delete(video_file.path)
+
+      # Publish the video to everyone
+      publish_video(video_uri)
+
       video_uri
     rescue VimeoMe2::RequestFailed => e
       Rails.logger.error "Vimeo upload failed: #{e.message}"
       nil
     end
   end
+
+  private
+  def publish_video(video_uri)
+    vimeo_video = VimeoMe2::Video.new(ENV.fetch("VIMEO_ACCESS_TOKEN"), video_uri)
+    vimeo_video.privacy = {view: 'anybody'}
+    vimeo_video.update
+  end
+
 end
