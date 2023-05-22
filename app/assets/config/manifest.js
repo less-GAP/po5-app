477c401cb0e65e5dd185e8551a498819ef697348
hungndl <hungndl86@gmail.com>
2023-05-22 23:27:37 +0700

    update stylesheet

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
 //= link_tree ../images
 // = link_directory ../stylesheets .css
 //= link_tree ../builds
-//= link spree/admin/videos.css
-//= link spree/backend/spree_admin.css
--- app/controllers/spree/admin/videos_controller.rb
+++ app/controllers/spree/admin/videos_controller.rb
@@ -1,25 +1,30 @@
-class Spree::Admin::VideosController < Spree::Admin::BaseController
+module Spree
+  module Admin
+    class VideosController < ResourceController
 
-  def index
-    @videos = Spree::Video.all
-  end
+      def index
+        @videos = Spree::Video.all
+      end
 
-  def new
+      def new
 
-  end
+      end
 
-  def create
-    @video_form = Spree::Admin::VideoForm.new(video_params)
-    if @video_form.save
-      redirect_to spree.admin_videos_path, notice: 'Video successfully created.'
-    else
-      render :new
+      def create
+        @video_form = Spree::Admin::VideoForm.new(video_params)
+        if @video_form.save
+          redirect_to spree.admin_videos_path, notice: 'Video successfully created.'
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def video_params
+        params.permit(:name, :description, :video_file)
+      end
     end
   end
-
-  private
-
-  def video_params
-    params.permit(:name, :description, :video_file)
-  end
 end
+
